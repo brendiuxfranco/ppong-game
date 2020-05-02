c7231d322c93bc05530a43c3753259265e75c978
brendiuxfranco <60198571+brendiuxfranco@users.noreply.github.com>
2020-05-01 19:21:07 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <v-app-bar app clipped-left dark color="red">
+      <v-toolbar-title>P-P-Pong Game</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer app clipped class="gray">
+      <v-list-item>
+        <v-list-item-content>
+          <v-spacer />
+          <v-list-item-title class="display-2">{{ $store.state.score }}</v-list-item-title>
+          <v-list-item-subtitle>Score</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider />
+
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">Game</v-list-item-title>
+          <v-list-item-subtitle>Settings</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider />
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-subtitle class="title">Ball</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list>
+        <v-list-item>
+          <v-slider v-model="$store.state.diameter" label="Diameter" color="success darken-2" track-color="success lighten-1" thumb-label max="200" min="50"></v-slider>
+        </v-list-item>
+        <v-list-item>
+          <v-slider v-model="$store.state.xBallChange" label="Speed X" color="success darken-2" track-color="success lighten-1" thumb-label max="10" min="-10"></v-slider>
+        </v-list-item>
+        <v-list-item>
+          <v-slider v-model="$store.state.yBallChange" label="Speed Y" color="success darken-2" track-color="success lighten-1" thumb-label max="10" min="-10"></v-slider>
+        </v-list-item>
+      </v-list>
+      <v-list>
+        <v-list-item>
+          <v-slider v-model="$store.state.paddleWidth" label="Width" color="success darken-2" track-color="success lighten-1" thumb-label max="300" min="10"></v-slider>
+        </v-list-item>
+        <v-list-item>
+          <v-slider v-model="$store.state.paddleSpeed" label="Speed" color="success darken-2" track-color="success lighten-1" thumb-label max="200" min="10"></v-slider>
+        </v-list-item>
+      </v-list>
+      <v-list-item>
+        <v-btn block color="primary" @click="$router.go(0)">Restart</v-btn>
+      </v-list-item>
+    </v-navigation-drawer>
+    <v-content class="error noOverflow">
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
